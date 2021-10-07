9651e4402bab983673b7edbcf46bf5835bae6ea3
Sadya <Klaar081@gmail.com>
2021-10-07 23:17:11 +0500

    some changes

--- dashboard/scrapers/ebayScraper.py
+++ dashboard/scrapers/ebayScraper.py
@@ -7,16 +7,14 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
 import os
-import sqlite3
 import sys
-from cgcValueScraper import get_values_and_grades
+# from scrapers.cgcValueScraper import get_values_and_grades
 import re
 import requests
 import cv2
 import difflib
 import random
 from selenium.webdriver.common.by import By
-import sqlite3
 
 
 class EbayScraper:
@@ -357,6 +355,42 @@ class EbayScraper:
                 os.remove(f'photo_temp/{self.product_title}_{suffix}.jpg')
             except Exception:
                 pass
+
+
+def get_values_and_grades(url):
+    try:
+        r = requests.get(url)
+        soup = BeautifulSoup(r.text, 'lxml')
+
+        try:
+            img_url = soup.find('section', class_='bg-white py-3 mb-3 shadow').find('div', class_='container').find(
+                'div', class_='col-3 col-md-2 col-xl-1').find('img').get('src')
+        except Exception:
+            img_url = None
+
+        soup = soup.find('div', class_='container mt-2').find('div', class_='col-12 col-md-6').find('div', class_='tab-pane active')
+        containers = soup.find('ul', class_='list-group mb-1').find_all('li', class_='list-group-item')[1:]
+        values_and_grades = []
+        try:
+            for container in containers:
+                try:
+                    container = container.find('div', class_='row')
+                    grade = float(container.find('div', class_='col-3 col-xl-2').text.strip())
+                    value = container.find('div', class_='col-5 col-xl-6 pr-xl-0 text-right').find('div', class_='col-12 col-xl-7')
+                    if value is None:
+                        value = container.find('div', class_='col-5 col-xl-6 pr-xl-0 text-right').find('div', class_='col-12 col-xl-7 text-muted')
+                    value = value.text.strip().rstrip('*').lstrip('$')
+                    value = float(''.join(value.split(',')).replace(',', '.'))
+                    values_and_grades.append((grade, value))
+                except Exception:
+                    pass
+        except Exception:
+            values_and_grades = None
+
+        return values_and_grades, img_url
+    except Exception:
+        return None, None
+
 
 
 if __name__ == "__main__":
